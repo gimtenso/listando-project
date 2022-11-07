3af1a325513083a18a4e3d3f8458091864ee0a9a
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 16:36:06 -0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name= "logout"),
-    path("sucesso", views.homepage, name="sucesso")
+    path("sucesso", views.sucesso, name="sucesso")
 ]
