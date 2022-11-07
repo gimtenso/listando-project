53e5a725feaeb01fe703c46be0af000963d59147
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 16:26:01 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
+
+from mongoengine import *
+connect('mongodb://user:user@ac-zvbacnb-shard-00-02.n4s7ayo.mongodb.net:27017/?ssl=true&replicaSet=atlas-89iqf1-shard-0&authSource=admin&retryWrites=true&w=majority')
+class User(Document):
+   mail=EmailField()
+   username = StringField()
+   password=StringField(min_value=8, max-value=20)
