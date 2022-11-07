2413bc0bea9c4fe302398b7e1f5950c7c09b0b21
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 03:19:54 -0300

    Update urls.py

--- listando/urls.py
+++ listando/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('https://listando.netlify.app/admin/', admin.site.urls),
-    path('https://listando.netlify.app/', include('main.urls')),
+    path('https://listando.herokuapp.com/admin/', admin.site.urls),
+    path('https://listando.herokuapp.com/', include('main.urls')),
 ]
