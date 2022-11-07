c8c88367ede3e679bc13415831bb492b66a4f9d3
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 03:19:31 -0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -8,9 +8,9 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("https://listando.netlify.app/", views.homepage, name="homepage"),
-    path("https://listando.netlify.app/register", views.register_request, name="register"),
-    path("https://listando.netlify.app/login", views.login_request, name="login"),
-    path("https://listando.netlify.app/logout", views.logout_request, name= "logout"),
-    path("https://listando.netlify.app/sucesso", views.homepage, name="sucesso")
+    path("https://listando.herokuapp.com/", views.homepage, name="homepage"),
+    path("https://listando.herokuapp.com/register", views.register_request, name="register"),
+    path("https://listando.herokuapp.com/login", views.login_request, name="login"),
+    path("https://listando.herokuapp.com/logout", views.logout_request, name= "logout"),
+    path("https://listando.herokuapp.com/sucesso", views.homepage, name="sucesso")
 ]
