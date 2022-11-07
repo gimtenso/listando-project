a778e732d1b8a550970242ba01ebf591193f359d
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 04:34:04 -0300

    Update settings.py

--- listando/settings.py
+++ listando/settings.py
@@ -11,13 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
