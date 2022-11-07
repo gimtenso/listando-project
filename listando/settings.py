cf26a5d1f731ccc9412399dc3749d348d32d909b
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-07 03:46:28 -0300

    Update settings.py

--- listando/settings.py
+++ listando/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_HOST = "https://listando.herokuapp.com/" if not DEBUG else ""
-STATIC_URL = STATIC_HOST + "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
