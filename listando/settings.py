3121cb0802e361262339e8b2a9372437a694d43a
FelipeMP94 <felipe00depaula@gmail.com>
2022-11-22 20:23:09 -0300

    comite para guithub

--- listando/settings.py
+++ listando/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import sys
-import django_heroku
+#import django_heroku
 import dj_database_url
 
 
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
