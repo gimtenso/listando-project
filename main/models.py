9abf52139d879aecc34c900a5e06cf8bdbf89d33
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-11-21 22:54:33 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,10 +1,19 @@
-from django.db import models
+from django_mongoengine import Document, EmbeddedDocument, fields
 
-from djangotoolbox.fields import ListField
+class Comment(EmbeddedDocument):
+    created_at = fields.DateTimeField(
+        default=datetime.datetime.now, editable=False,
+    )
+    author = fields.StringField(verbose_name="Name", max_length=255)
+    email  = fields.EmailField(verbose_name="Email")
+    body = fields.StringField(verbose_name="Comment")
 
-
-class Post(models.Model):
-    title = models.CharField()
-    text = models.TextField()
-    tags = ListField()
-    comments = ListField()
+class Post(Document):
+    created_at = fields.DateTimeField(
+        default=datetime.datetime.now, editable=False,
+    )
+    title = fields.StringField(max_length=255)
+    slug = fields.StringField(max_length=255, primary_key=True)
+    comments = fields.ListField(
+        fields.EmbeddedDocumentField('Comment'), blank=True,
+    )
