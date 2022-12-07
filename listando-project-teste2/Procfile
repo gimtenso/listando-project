46f5c9eb940bce0666afd0899fe410680c7ddce6
gimtenso <82822115+gimtenso@users.noreply.github.com>
2022-12-07 14:27:09 -0300

    Add files via upload

---
+++ listando-project-teste2/Procfile
@@ -0,0 +1,1 @@
+web gunicorn listando.wsgi:application 
---
+++ listando-project-teste2/main/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect, HttpResponse
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.views.generic import TemplateView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+
+from . import geraPDF
+import io
+
+
+def homepage(request):
+	return render(request=request, template_name='main/index.html')
+
+
+def sucesso(request, filename=''):
+	if filename != '':
+		buffer = io.BytesIO()
+
+		assunto = request.POST.get("tema")
+		#quantidade = request.POST.get("quantidade")
+		geraPDF.gerarPDF(assunto)
+
+		response = HttpResponse(content_type='application/pdf')
+		response['Content-Disposition'] = 'attachment; filename=lista_personalizada.pdf'
+		response.write(buffer.getvalue())
+		buffer.close()
+
+		return response
+	else:
+		return render(request, 'main/sucesso.html')
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful.")
+			return redirect("main:login")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="main/register.html", context={"register_form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:sucesso")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
+
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("main:homepage")
