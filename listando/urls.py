aa264ef2dd62b7d682679cf7d9cebf3d8c8e5a04
anamueller <mm.anaclara@gmail.com>
2022-12-07 14:21:16 -0300

    consertei eu acho - ana

--- listando/urls.py
+++ listando/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
